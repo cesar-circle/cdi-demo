4681428a257d993ae70eafa6727db29014766144
Cesar Garcia <cesar.garciaalesanco@circle.com>
2024-03-15 13:31:04 +0000

    Create MessagePoller beans after all other beans have been discovered

--- src/main/java/com/circle/cesar/cdi/extension/DemoExtension.java
+++ src/main/java/com/circle/cesar/cdi/extension/DemoExtension.java
@@ -1,7 +1,10 @@
 package com.circle.cesar.cdi.extension;
 
+import com.circle.cesar.cdi.MessagePoller;
 import com.circle.cesar.cdi.QueueName;
+import com.circle.cesar.cdi.QueueNameLiteral;
 import com.circle.cesar.cdi.Worker;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.inject.spi.AfterBeanDiscovery;
 import jakarta.enterprise.inject.spi.AfterDeploymentValidation;
@@ -12,6 +15,7 @@ import jakarta.enterprise.inject.spi.BeforeShutdown;
 import jakarta.enterprise.inject.spi.Extension;
 import jakarta.enterprise.inject.spi.ProcessBean;
 
+import java.lang.annotation.Annotation;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -21,38 +25,58 @@ public class DemoExtension implements Extension {
 
     //region Container Lifecycle
     public void beforeBeanDiscovery(@Observes BeforeBeanDiscovery bfd) {
-        System.out.println("Before bean discovery");
+        System.out.println("LIFECYCLE: Before bean discovery");
     }
 
     public void afterTypeDiscovery(@Observes AfterTypeDiscovery atd) {
-        System.out.println("After type discovery");
-    }
-
-    public void afterBeanDiscover(@Observes AfterBeanDiscovery abd) {
-        System.out.println("After bean discovery");
+        System.out.println("LIFECYCLE: After type discovery");
     }
 
     public void afterDeploymentValidation(@Observes AfterDeploymentValidation adv) {
-        System.out.println("After deployment validation discovery");
+        System.out.println("LIFECYCLE: After deployment validation discovery");
     }
 
     public void beforeShutdown(@Observes BeforeShutdown bs) {
-        System.out.println("Before shutdown");
+        System.out.println("LIFECYCLE: Before shutdown");
     }
     //endregion
 
-    public void processInjectionPoint(@Observes ProcessBean<? extends Worker> event) {
+    public void collectWorkers(@Observes ProcessBean<? extends Worker> event) {
         getQueueName(event).ifPresent(queueName -> {
             System.out.printf("Registered worker for queue '%s'%n", queueName);
             workers.put(queueName, event.getBean());
         });
     }
 
+    public void createPollers(@Observes AfterBeanDiscovery abd) {
+        System.out.println("LIFECYCLE: After bean discovery");
+        for(var entry : workers.entrySet()) {
+            final String queueName = entry.getKey();
+            System.out.printf("Registering worker bean on queue '%s'%n", queueName);
+            registerProducerBean(abd, queueName, entry.getValue());
+        }
+    }
+
+    private void registerProducerBean(AfterBeanDiscovery event, String queueName, Bean<? extends Worker> workerBean) {
+        System.out.printf("Creating MessagePoller instance for worker on queue '%s'%n", queueName);
+        event.addBean()
+            .beanClass(MessagePoller.class)
+            .types(MessagePoller.class)
+            .scope(ApplicationScoped.class)
+            .qualifiers(QueueNameLiteral.withName(queueName))
+            .produceWith(instance -> {
+                var worker = instance.select(
+                    workerBean.getBeanClass(),
+                    workerBean.getQualifiers().toArray(Annotation[]::new)
+                ).get();
+                return new MessagePoller(queueName, (Worker) worker);
+            });
+    }
+
+
     private static Optional<String> getQueueName(ProcessBean<? extends Worker> event) {
         return Optional.ofNullable(
             event.getAnnotated().getAnnotation(QueueName.class)
         ).map(QueueName::value);
     }
-
-
 }
